f582f2c01fc88c2fd3ba60d11b331cf258c09913
Ellen Hatleberg <ellen.hatleberg@gmail.com>
2018-11-13 19:32:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,8 +24,10 @@ function currentLine(line) {
   if (line.length === 0) {
     return `The line is currently empty.`;
   } else {
-    return `The line is currently: 1. Ada, 2. Grace`
+    for (let i = 0; i < line.length; i++) {
+      
+      return `The line is currently: 1. Ada, 2. Grace`
+    };
   };
-
 };
 currentLine();
